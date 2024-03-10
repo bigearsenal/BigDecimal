2175170080c4c0d6ea143a9e797d3ae2da2ac48d
Chung Tran <bigearsenal@gmail.com>
2024-03-10 17:40:37 +0700

    Update package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.4
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,13 +9,14 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "BigDecimal",
-            targets: ["BigDecimal"]),
+            targets: ["BigDecimal"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/attaswift/BigInt.git", from: "5.3.0"),
-        .package(url: "https://github.com/apple/swift-numerics.git", from: "1.0.0")
+        .package(url: "https://github.com/apple/swift-numerics.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -24,14 +25,14 @@ let package = Package(
             name: "BigDecimal",
             dependencies: [
                 "BigInt",
-                .product(name: "Numerics", package: "swift-numerics")
+                .product(name: "Numerics", package: "swift-numerics"),
             ]
         ),
         .testTarget(
             name: "BigDecimalTests",
             dependencies: [
                 "BigDecimal",
-                "BigInt"
+                "BigInt",
             ]
         ),
     ]
