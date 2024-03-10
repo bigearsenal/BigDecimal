911d34c7a3e4336d6edd2bba6f60a18fc5fac7db
Chung Tran <bigearsenal@gmail.com>
2024-03-10 18:39:47 +0700

    feat: impor Numerics

--- Sources/BigDecimal/RangeReplaceableCollection+removeWhile.swift
+++ Sources/BigDecimal/RangeReplaceableCollection+removeWhile.swift
@@ -1,15 +1,17 @@
+import Numerics
+
 extension RangeReplaceableCollection {
     @usableFromInline
-     internal mutating func remove(while predicate: (Element) throws -> Bool) rethrows {
+    mutating func remove(while predicate: (Element) throws -> Bool) rethrows {
         guard let index = try indices.first(where: { try !predicate(self[$0]) }) else {
             removeAll()
             return
         }
         removeSubrange(..<index)
     }
-    
+
     @usableFromInline
-    internal mutating func removeLast(while predicate: (Element) throws -> Bool) rethrows {
+    mutating func removeLast(while predicate: (Element) throws -> Bool) rethrows {
         guard let index = try indices.reversed().first(where: { try !predicate(self[$0]) }) else {
             removeAll()
             return
